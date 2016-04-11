c22f661e30af2b136b6f71a2afbfea556b15cd06
Timur Ullman Önener <timur.onener@gmail.com>
2016-04-11 23:58:56 +0200

    Modified the project folder
    The project is a C# project hence the change and not just the code files
    in it anymore.

---
+++ CookieFinder/CookieFinder.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace CookieFinder
+{
+    partial class CookieFinder
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtInputURL = new System.Windows.Forms.TextBox();
+            this.btnFind = new System.Windows.Forms.Button();
+            this.lblURL = new System.Windows.Forms.Label();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtInputURL
+            // 
+            this.txtInputURL.Location = new System.Drawing.Point(12, 64);
+            this.txtInputURL.Name = "txtInputURL";
+            this.txtInputURL.Size = new System.Drawing.Size(422, 20);
+            this.txtInputURL.TabIndex = 0;
+            this.txtInputURL.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtInputURL_KeyPress);
+            // 
+            // btnFind
+            // 
+            this.btnFind.Location = new System.Drawing.Point(451, 62);
+            this.btnFind.Name = "btnFind";
+            this.btnFind.Size = new System.Drawing.Size(75, 23);
+            this.btnFind.TabIndex = 1;
+            this.btnFind.Text = "&Hitta";
+            this.btnFind.UseVisualStyleBackColor = true;
+            this.btnFind.Click += new System.EventHandler(this.btnFind_Click);
+            // 
+            // lblURL
+            // 
+            this.lblURL.AutoSize = true;
+            this.lblURL.Location = new System.Drawing.Point(12, 48);
+            this.lblURL.Name = "lblURL";
+            this.lblURL.Size = new System.Drawing.Size(104, 13);
+            this.lblURL.TabIndex = 2;
+            this.lblURL.Text = "Adress att söka efter";
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(451, 118);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 5;
+            this.btnExit.Text = "&Avsluta";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(15, 101);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(0, 13);
+            this.lblResult.TabIndex = 4;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(12, 118);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 3;
+            this.btnDelete.Text = "Ta &bort!";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Visible = false;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // CookieFinder
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(536, 153);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.lblURL);
+            this.Controls.Add(this.btnFind);
+            this.Controls.Add(this.txtInputURL);
+            this.Name = "CookieFinder";
+            this.Text = "CookieFinder by Timur";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtInputURL;
+        private System.Windows.Forms.Button btnFind;
+        private System.Windows.Forms.Label lblURL;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Button btnDelete;
+    }
+}
+
